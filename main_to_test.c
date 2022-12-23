a7fc4bd81ff16c4535000b6b4f0f7dc906239f32
tmarts <tmarts@student.42heilbronn.de>
2022-12-23 22:02:01 +0100

    files to test

--- main_to_test.c
+++ main_to_test.c
@@ -6,7 +6,7 @@
 /*   By: tmarts <tmarts@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 18:53:36 by tmarts            #+#    #+#             */
-/*   Updated: 2022/12/15 22:35:41 by tmarts           ###   ########.fr       */
+/*   Updated: 2022/12/23 21:48:39 by tmarts           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,29 +19,52 @@ char	*get_next_line(int fd);
 
 int	main (void)
 {
-	// int		fd1;
-	// char	*str;
-	// int		fd2;
+	int		fd1;
+	char	*str;
+	int		fd2;
+	char	*str2;
 	int		fd3;
-	// char	*str2;
 	char	*str3;
-
-	// fd1 = open("marie_under.txt", O_RDONLY);
-	// str = get_next_line(fd1);
-	// printf("The next line is: %s", str);
-	// free(str);
-	// str = get_next_line(fd1);
-	// printf("The next line is: [%s]", str);
-	// free(str);
-	// str = get_next_line(fd1);
-	// printf("The next line is: [%s]", str);
-	// free(str);
-	// fd2 = open("neuromancer.txt", O_RDONLY);
+	
+	fd1 = open("marie_under.txt", O_RDONLY);
+	str = get_next_line(fd1);
+	printf("The next line is: [%s]", str);
+	free(str);
+	str = get_next_line(fd1);
+	printf("The next line is: [%s]", str);
+	free(str);
+	str = get_next_line(fd1);
+	printf("The next line is: [%s]", str);
+	free(str);
+	str = get_next_line(fd1);
+	printf("The next line is: [%s]", str);
+	free(str);
+	str = get_next_line(fd1);
+	printf("The next line is: [%s]", str);
+	free(str);
+	str = get_next_line(fd1);
+	printf("The next line is: [%s]", str);
+	free(str);
+	printf("_________________________________________\n");
+	fd2 = open("neuromancer.txt", O_RDONLY);
+	str2 = get_next_line(fd2);
+	printf("The next line is: [%s]", str2);
+	free(str2);
+	str2 = get_next_line(fd2);
+	printf("The next line is: [%s]", str2);
+	free(str2);
+	str2 = get_next_line(fd2);
+	printf("The next line is: [%s]", str2);
+	free(str2);
+	printf("_________________________________________\n");
 	fd3 = open("notext.txt", O_RDONLY);
-	// str2 = get_next_line(fd2);
 	str3 = get_next_line(fd3);
-	// printf("The next line is: [%s]", str2);
 	printf("The next line is: [%s]", str3);
-	// free(str2);
+	free(str3);
+	str3 = get_next_line(fd3);
+	printf("The next line is: [%s]", str3);
+	free(str3);
+	str3 = get_next_line(fd3);
+	printf("The next line is: [%s]", str3);
 	free(str3);
 }
